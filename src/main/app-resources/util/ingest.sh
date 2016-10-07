7d8869e0aa2cf6174eee2645867b3fc0b643e32d
Cesare Rossi <cesare.rossi@terradue.com>
2016-10-08 00:31:05 +0200

    Added ingest tool

---
+++ src/main/app-resources/util/ingest.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###############################################################################
+# Copy the Opensearch entry from the local Sandbox Catalogue to a remote one.
+# Globals:
+#   WORKFLOW
+#   APIKEY
+#   HOSTNAME
+#   USER
+###############################################################################
+
+# Local variables
+remote_catalog=catalog.terradue.com
+index=melodies-training
+base_url="https://${remote_catalog}/${index}"
+
+# Get the local atom response
+xml=/tmp/atom.$$.xml
+opensearch-client -m EOP "http://$HOSTNAME/sbws/wps/sen2cor/${WORKFLOW}/results/search" {} > ${xml}
+
+# Compute the identifier of the entry
+identifier=$( xmlstarlet sel -N x="http://www.w3.org/2005/Atom" -N y="http://purl.org/dc/elements/1.1/" -t -v "/x:feed/x:entry/y:identifier" ${xml})
+
+# Update the atom values
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:link[@rel='alternate']/@href" -v "${base_url}/?count=20&amp;format=atom" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:link[@rel='search']/@href" -v "${base_url}/description" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:link[@rel='self']/@href" -v "${base_url}/search?count=20&amp;format=atom" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:entry/x:id" -v "${base_url}/search?format=atom&amp;id=${identifier}" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:entry/x:link[@rel='enclosure']/@href" -v "https://store.terradue.com/${index}/${USER}/${identifier}" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:entry/x:link[@rel='self']/@href" -v "${base_url}/search?format=atom&amp;id=${identifier}" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -u "/x:feed/x:entry/x:link[@rel='search']/@href" -v "${base_url}/description" ${xml}
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -N y="http://purl.org/dc/elements/1.1/" -u "/x:feed/x:entry/y:parentIdentifier" -v "${base_url}/description" ${xml}
+
+# Set properly the WMS offering
+layer_id="${index}:${USER}-${HOSTNAME}-${identifier}"
+wms="http://geoserver-melodies.terradue.com/geoserver/melodies-training/wms?service=WMS&version=1.1.0&request=GetMap&layers=${layer_id}&styles=&bbox=300000.0,6490200.0,409800.0,6600000.0&width=768&height=768&srs=EPSG:32632&format=image%2Fpng"
+xmlstarlet ed -L -N x="http://www.w3.org/2005/Atom" -N y="http://www.opengis.net/owc/1.0" -u "/x:feed/x:entry/y:offering/y:content/@href" -v "${wms}" ${xml}
+
+# Finally upload the entry
+curl -u ${USER}:${APIKEY} -XPOST -H "Content-Type: application/atom+xml" -d@${xml} "https://catalog.terradue.com/${index}"
+
+exit 0
